ad1192a0295fc1f08a1e0e6f6ba7a16ccdca26ac
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2026-06-28 22:52:20 +0900

    feat : Inventory

--- MobileMergeProject/Assets/_Code/Inventory/Inventory.cs
+++ MobileMergeProject/Assets/_Code/Inventory/Inventory.cs
@@ -1,16 +1,45 @@
-using UnityEngine;
+using System.Collections.Generic;
+using _Code.GunData;
+using _Code.Manager;
 
-public class Inventory : MonoBehaviour
+namespace Code.Inventories
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class Inventory : MonoSingleton<Inventory>
     {
-        
-    }
+        private Dictionary<string, List<GunDataSO>> gunDatas = new();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void GetGun(GunDataSO gunData)
+        {
+            if (gunData == null)
+                return;
+
+            string gunName = gunData.GunName;
+
+            if (!gunDatas.TryGetValue(gunName, out List<GunDataSO> guns))
+            {
+                guns = new List<GunDataSO>();
+                gunDatas.Add(gunName, guns);
+            }
+
+            guns.Add(gunData);
+        }
+
+        public void Remove(GunDataSO gunData)
+        {
+            if (gunData == null)
+                return;
+
+            string gunName = gunData.GunName;
+
+            if (!gunDatas.TryGetValue(gunName, out List<GunDataSO> guns))
+                return;
+
+            guns.Remove(gunData);
+
+            if (guns.Count == 0)
+            {
+                gunDatas.Remove(gunName);
+            }
+        }
     }
 }
